6fb06781ae68579cd1f38d172840fae5346b7049
iThinkyouknow <woontlt.dev@gmail.com>
2021-05-08 20:46:22 +0800

    added the-script to be the script to run them ALL

---
+++ data-scripts/the-script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#  This is THE script.js
+# The script that you need to run to
+#   1) auto generate the search json
+#   2) generate minified jsons
+node ./create-search-psalter-data.js
+node ./data-minifier.js
