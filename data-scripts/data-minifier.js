fd050de82ee1d313853717ac54903f5d1d42592e
iThinkyouknow <woontlt.dev@gmail.com>
2021-05-08 20:35:19 +0800

    modified data-minifier to handle minifying from _ files and deminifying to minified files

--- data-scripts/data-minifier.js
+++ data-scripts/data-minifier.js
@@ -1,40 +1,49 @@
 const fs = require('fs');
+const minifiedFilesRegex = /\.json$/i;
+const unminifiedFilesRegex = /_\S+\.json$/i;
 
-const fileNames = fs
-    .readdirSync(`${__dirname}/../`)
-    .filter((fileName) => /\.json$/i.test(fileName));
-
-
+const getFileNames = (regex = minifiedFilesRegex) => {
+    return fs
+        .readdirSync(`${__dirname}/../`)
+        .filter((fileName) => regex.test(fileName));
+};
 
 const apply = (val, fn) => fn(val);
 
 const compose = (fns) => (val) => fns.reduce(apply, val);
 
 const getFile = (fileName) => ({obj: require(`../${fileName}`), fileName});
+const setDestinationFileName = (transformFn = (name) => name) => ((obj) => {
+    obj.fileName = transformFn(obj.fileName);
+    return obj;
+});
+const removePrefixFromName = (name) => name.slice(1);
 const stringify = (indent) => ({obj, fileName}) => ({str: JSON.stringify(obj, null, indent), fileName});
-const writeToFile = ({str, fileName}) => fs.writeFileSync(`${__dirname}/../${fileName}`, str);
-
+const _writeToFile = (filePrefix = '') => ({ str, fileName }) => fs.writeFileSync(`${__dirname}/../${filePrefix}${fileName}`, str);
+const writeToFile = _writeToFile();
+const writeToFilePrefix = _writeToFile('_');
 const trace = (obj) => console.dir(obj, {colors: true, depth: null});
 
-trace(fileNames);
 
 const minify = (fileNames) => fileNames
-    .map(compose([
-        getFile
-        , stringify()
-        , writeToFile
-    ]));
+.map(compose([
+    getFile
+    , setDestinationFileName(removePrefixFromName)
+    , stringify()
+    , writeToFile
+]));
 
 const deminify = (fileNames) => fileNames
-    .map(compose([
-        getFile,
-        stringify(4)
-        , writeToFile
-    ]));
+.map(compose([
+    getFile
+    , stringify(4)
+    , writeToFilePrefix
+]));
 
-
-// minify(fileNames);
-// deminify(fileNames);
+trace(getFileNames(unminifiedFilesRegex));
+minify(getFileNames(unminifiedFilesRegex));
+// trace(getFileNames(minifiedFilesRegex));
+// deminify(getFileNames(minifiedFilesRegex));
 
 
 
