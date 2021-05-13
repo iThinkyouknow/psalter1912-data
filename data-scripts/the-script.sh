45286f71f67a8c0498a6461ae66f334c717da52b
iThinkyouknow <woontlt.dev@gmail.com>
2021-05-14 01:05:09 +0800

    reset version  to 1

--- data-scripts/the-script.sh
+++ data-scripts/the-script.sh
@@ -3,6 +3,5 @@
 # The script that you need to run to
 #   1) auto generate the search json
 #   2) generate minified jsons
-node automagically-updating-version-json.js $(git diff --name-only --cached)
 node ./create-search-psalter-data.js
 node ./data-minifier.js
